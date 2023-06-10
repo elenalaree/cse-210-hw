3692f92a2e474f0c6028d3c1e634f9c46d463644
Elena Carey <theelenacarey@gmail.com>
2023-06-10 13:19:54 -0500

    Finishing prep 3.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        Console.Write(number);
+        int guess = 0;
+        int count = 0;
+        while (number != guess)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            count++;
+            Console.WriteLine($"You have guessed {count} times");
+            if (number < guess)
+            {
+                Console.WriteLine("Guess lower.");
+            }
+            else if( number > guess)
+            {
+                Console.WriteLine("Guess higher.");
+            }
+            else
+            {
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine("Do you want to guess again? ");
+            string answer = Console.ReadLine();
+            if(answer == "yes")
+            {
+                number = randomGenerator.Next(1, 101);
+            }
+            else{
+                Console.WriteLine("Thanks for playing!");
+            }
+            }
+        }
     }
 }
