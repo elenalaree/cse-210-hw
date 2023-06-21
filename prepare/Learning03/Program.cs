ef0763b83fe85eb3b26d4a1240ae68ab5edc85b8
Elena Carey <theelenacarey@gmail.com>
2023-06-21 17:16:16 -0500

    Finishing learning 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction(6);
+        Fraction f2 = new Fraction(2, 3);
+        Fraction f3 = new Fraction();
+        Fraction f4 = new Fraction(5, 8);
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
